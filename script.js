7c95cf965db724dbee63477edb140bbc1501cf5b
eskainah <eskainah@gmail.com>
2024-03-24 20:47:42 +0200

    write function for slider

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ menuLinks.forEach(link => {
     menu.classList.remove('active');
   });
 });
+
+//Slide show starts here
+const slider = document.querySelector('.container');
+let isDown = false;
+let startX, scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+})
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3;
+
+    slider.scrollLeft = scrollLeft - walk;
+})
